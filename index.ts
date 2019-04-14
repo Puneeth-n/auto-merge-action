d49c76769f9215ed42c6cd7968f7573fa76b9eff
Puneeth <puneeth.nanjundaswamy@comtravo.com>
2019-04-14 15:56:31 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,5 @@
 import { Toolkit } from 'actions-toolkit';
 
-const tools = new Toolkit({
-  event: ['release.published']
-});
-
-(async () => {
-    console.log(JSON.stringify(tools.context.payload));
-    tools.exit.success('Please set environment variable CONFLICT_LABEL_NAME');
-  }
-)
+const tools = new Toolkit();
+console.log(JSON.stringify(tools.context.payload));
+tools.exit.success('Please set environment variable CONFLICT_LABEL_NAME');
