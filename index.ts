e9a022cf76efa5fff2eea448d3e2124f43835587
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2019-04-17 00:03:38 +0200

    nits

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
 import * as Octokit from '@octokit/rest'
 import { Toolkit } from 'actions-toolkit';
+import { get, intersection } from 'lodash';
 
 const toolkit = new Toolkit({
   event: ['pull_request', 'release', 'check_run.completed']
 });
 
 const okToMergeLabel: string = process.env.OK_TO_MERGE_LABEL!;
-
 const baseBranch: string = process.env.BASE_BRANCH!;
+const octokit = toolkit.github as Octokit;
+const blacklistLabels: string[] = get(process.env, 'BLACKLIST_LABELS', '').split(',')
+const waitMs: number = 5000;
+const maxRetries: number = 5;
 
+
+toolkit.log.info(`Using blacklist labels ${blacklistLabels}`);
 
 if (!okToMergeLabel) {
   toolkit.exit.failure('Please set environment variable OK_TO_MERGE_LABEL');
@@ -19,48 +25,121 @@ if (!baseBranch) {
 }
 
 
-const handleReleaseEvents = async (tools: Toolkit) => {
-  tools.log.info('Handling GitHub release event');
-  tools.log.info(JSON.stringify(tools.context));
+const wait = (ms: number): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-  const context = tools.context.repo;
 
-  const octokit = tools.github as Octokit;
+const fetchAllPullRequests = async (): Promise<Octokit.Response<Octokit.PullsListResponseItem[]>> => {
+  toolkit.log.info('Fetchling all pull requests');
 
-  const pulls = await octokit.pulls.list({
+  const repo = toolkit.context.repo;
+
+  return octokit.pulls.list({
     base: baseBranch,
-    owner: context.owner,
+    owner:repo.owner,
     per_page: 100,
-    repo: context.repo,
+    repo: repo.repo,
     sort: 'updated',
     state: 'open'
+  })
+}
+
+const filterPullRequestsByLabel = (allPullRequests: Octokit.Response<Octokit.PullsListResponseItem[]>): Octokit.PullsListResponseItem[] => allPullRequests.data.filter((pullRequest) => {
+  const assignedLabels: string[] = [];
+  const whitelistLabel: string[] = [okToMergeLabel];
+  pullRequest.labels.forEach((assignedLabel) => {
+    assignedLabels.push(assignedLabel.name);
+  });
+  if (intersection(assignedLabels, whitelistLabel).length === 1 &&
+    intersection(assignedLabels, blacklistLabels).length === 0) {
+    return true;
+  }
+  return false;
+})
+
+const getPullRequest = async (pullRequestNumber: number, tries:number = 0): Promise<Octokit.Response<Octokit.PullsGetResponse>> => {
+
+  if (tries > maxRetries) {
+    throw new Error(`Error fetching pull request number: ${pullRequestNumber}`);
+  }
+
+  const repo = toolkit.context.repo;
+
+  const pullRequestResponse =  await octokit.pulls.get({
+    owner:repo.owner,
+    pull_number: pullRequestNumber,
+    repo: repo.repo
   });
 
-  tools.log.info(JSON.stringify(pulls));
+  const mergable = pullRequestResponse.data.mergeable;
+
+  if (mergable === null) {
+    await wait(waitMs);
+    tries++;
+    return getPullRequest(pullRequestNumber, tries);
+  }
+
+  return pullRequestResponse
 }
 
-const handlePullRequestEvents = async (tools: Toolkit) => {
-  tools.log.info('Handling GitHub pull request event');
-  tools.log.info(JSON.stringify(tools.context));
+
+const handleReleaseEvents = async (): Promise<void> => {
+  toolkit.log.info('Handling GitHub release event');
+  toolkit.log.info(toolkit.context);
+
+  try {
+    const allPullRequests = await fetchAllPullRequests();
+    const eligiblePullRequests = filterPullRequestsByLabel(allPullRequests);
+
+    if (eligiblePullRequests.length === 0) {
+      toolkit.exit.neutral('No pull requests ot process');
+    }
+
+    const promises: Array<Promise<Octokit.Response<Octokit.PullsGetResponse>>> = [];
+
+    eligiblePullRequests.forEach((pullRequest) => {
+      promises.push(getPullRequest(pullRequest.number));
+    })
+
+    try {
+      const pullRequests = await Promise.all(promises);
+      toolkit.log.info(pullRequests)
+      toolkit.exit.success('success');
+    } catch(error) {
+      toolkit.exit.failure(`Error processing pullRequests ${error}`);
+    }
+
+  }
+  catch(error) {
+    toolkit.exit.failure('Error processing GitHub release event!!!');
+  }
 }
 
-const handleCheckRunEvents = async (tools: Toolkit) => {
-  tools.log.info('Handling GitHub check_run event');
-  tools.log.info(tools.context);
+const handlePullRequestEvents = async (): Promise<void> => {
+  toolkit.log.info('Handling GitHub pull request event');
+  toolkit.log.info(toolkit.context);
+}
+
+const handleCheckRunEvents = async (): Promise<void> => {
+  toolkit.log.info('Handling GitHub check_run event');
+  toolkit.log.info(toolkit.context);
 }
 
 
 switch (toolkit.context.event) {
   case "release":
-    handleReleaseEvents(toolkit);
+    handleReleaseEvents();
     break;
 
   case "labeled":
-    handlePullRequestEvents(toolkit);
+    handlePullRequestEvents();
     break;
 
   case "check_run":
-    handleCheckRunEvents(toolkit);
+    handleCheckRunEvents();
     break;
 
   default:
